245e0029542cbb77d99bdc5dfe20537f0fb7a2a6
Fernando Salom <Fdosalom@gmail.com>
2022-10-13 10:55:06 +0200

    Add protocol for JustChatManager

--- Sources/JustChat/JustChat.swift
+++ Sources/JustChat/JustChat.swift
@@ -3,9 +3,37 @@ import FirebaseFirestore
 import FirebaseAuth
 
 public class JustChatManager {
-    var shared = JustChatManager()
-    
-    public init() {
+    var dataSource: DataSourceProtocol
+
+    public init(with dataSource: DataSourceProtocol) {
+        self.dataSource = dataSource
         FirebaseApp.configure()
+    }
+
+}
+
+extension JustChatManager: DataSourceProtocol {
+    public func getChats(with parameters: [String : Any]) async throws -> [ChatProtocol] {
+        do {
+            return try await self.dataSource.getChats(with: parameters)
+        }catch{
+            throw error
+        }
+    }
+
+    public func getChat(with id: String) async throws -> ChatProtocol {
+        do {
+            return try await self.dataSource.getChat(with: id)
+        }catch{
+            throw error
+        }
+    }
+
+    public func send(this message: ChatMessageProtocol) async throws {
+        do {
+            return try await self.dataSource.send(this: message)
+        }catch{
+            throw error
+        }
     }
 }
