ac80769eadd371970bdcad1d3f1bfdeafd9ebbbb
Fernando Salom <Fdosalom@gmail.com>
2022-10-13 10:26:08 +0200

    Add chat initializer

--- Sources/JustChat/Data/Chat/Chat.swift
+++ Sources/JustChat/Data/Chat/Chat.swift
@@ -2,4 +2,10 @@ class Chat {
     var id: String
     var name: String
     var group: String
+
+    init(id: String, name: String, group: String) {
+        self.id = id
+        self.name = name
+        self.group = group
+    }
 }
